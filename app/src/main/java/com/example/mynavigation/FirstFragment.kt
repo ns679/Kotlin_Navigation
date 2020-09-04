4deb7228ce35e1db7c8b912eaf2cc5b5bbc9b574
aaa165 <b1802067@planet.kanazawa-it.ac.jp>
2020-09-04 10:22:49 +0900

    Add_All

---
+++ app/src/main/java/com/example/mynavigation/FirstFragment.kt
@@ -0,0 +1,39 @@
+package com.example.mynavigation
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.navOptions
+
+/**
+ * A simple [Fragment] subclass as the default destination in the navigation.
+ */
+class FirstFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_first, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val options = navOptions {
+            anim {
+                enter = R.anim.slide_in_right
+                exit = R.anim.slide_out_left
+                popEnter = R.anim.slide_in_left
+                popExit = R.anim.slide_out_right
+            }
+        }
+        view.findViewById<Button>(R.id.button_first)?.setOnClickListener {
+            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment2,null,options)
+        }
+    }
+}
